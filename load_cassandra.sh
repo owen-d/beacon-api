3dd8711721dea3e57b94eda96ae119d8483136b3
owen <ow.diehl@gmail.com>
2017-06-15 18:48:22 -0400

    makes data injection optional in cassandra bootstrapping script

--- load_cassandra.sh
+++ load_cassandra.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+CQL_VERSION=${CQL_VERSION-3.4.4}
 set -e
 
+#Keyspace is inlined as it must be called first
 KEYSPACE_STR="$(cat <<EOF
 CREATE KEYSPACE IF NOT EXISTS bkn
 WITH REPLICATION = { 
@@ -11,6 +13,14 @@ WITH REPLICATION = {
 EOF
 )"
 
+# idempotent structure creation
+cat <(find ./cass/structure -type f | xargs cat <(echo $KEYSPACE_STR)) | cqlsh --cqlversion=$CQL_VERSION
 
-cat <(find ./cass/ -type f | xargs cat <(echo $KEYSPACE_STR)) | cqlsh --cqlversion=3.4.4
+# unless NO_DATA var is present, inject data as well
+if [[ -z $NO_DATA ]];
+    then
+        cat <(find ./cass/data -type f | xargs cat) | cqlsh --cqlversion=$CQL_VERSION
+    fi
+
+
 
