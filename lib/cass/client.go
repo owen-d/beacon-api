428d1269752ea389fc1c107856f391ace244810c
owen <ow.diehl@gmail.com>
2017-06-16 12:19:39 -0400

    building out cassandra lib

--- lib/cass/client.go
+++ lib/cass/client.go
@@ -1,7 +1,9 @@
 // Cassandra lib
 package cass
 
-import ()
+import (
+	"github.com/gocql/gocql"
+)
 
 // interface for exported functionality
 type Client interface {
@@ -12,43 +14,71 @@ type Client interface {
 	// Schedule
 }
 
-type Message struct {
-	UserId int
-	Name   string
-	Title  string
-	Url    string
-	Lang   string
+type User struct {
+	Id    gocql.UUID `cql:"id"`
+	email string     `cql:"email"`
 }
 
+type Beacon struct {
+	UserId      gocql.UUID `cql:"user_id"`
+	DeployName  string     `cql:"deploy_name"`
+	MessageName string     `cql:"message_name"`
+}
+
+type Message struct {
+	UserId      gocql.UUID `cql:"user_id"`
+	Name        string     `cql:"name"`
+	Title       string     `cql:"title"`
+	Url         string     `cql:"url"`
+	Lang        string     `cql:"lang"`
+	Deployments []string   `cql:"deployments"`
+}
+
+// Deployment is not an actual data structure stored in cassandra, but rather a construct that we disassemble into beacons. If a MessageId is provided, we will read/use that
+// for settting a deployment method, otherwise creating a message if the Message field is set.
 type Deployment struct {
-	UserId      uuid
+	UserId      gocql.UUID
 	DeployName  string
+	MessageId   string
 	Message     *Message
 	BeaconNames []string
 }
 
-type Beacon struct {
-	UserId      uuid
-	DeployName  string
-	MessageName string
-}
-
 type CassClient struct{}
 
-// UpsertDeployment writes the current deployname to every beacon in the list via an update clause, causing any now-invalid records in the deployments materialized view (on top of beacons) to be deleted via a partition drop (& subsequently recreated)
-// Must write Deployment to every beacon
-func (self *CassClient) UpsertDeployment() {
-	// take list of beacon names, write the new deployname to them all
+// Users ------------------------------------------------------------------------------
 
-	// if message
+func (self *CassClient) CreateUser(u *User) {}
+func (self *CassClient) FetchUser(u *User)  {}
+
+// Beacons ------------------------------------------------------------------------------
+
+func (self *CassClient) CreateBeacons(beacons []*Beacon) {}
+func (self *CassClient) UpdateBeacons(beacons []*Beacon) {}
+
+// Messages ------------------------------------------------------------------------------
+
+func (self *CassClient) CreateMessage(m *Message) {}
+func (self *CassClient) ChangeMessageDeployments(m *Message, additions *Message.Deployments, removals *Message.Deployments) {
+}
+
+// Deployments ------------------------------------------------------------------------------
+
+// PostDeployment writes the current deployname to every beacon in the list via an update clause, causing any now-invalid records in the deployments materialized view (on top of beacons) to be deleted via a partition drop (& subsequently recreated)
+// Must write Deployment to every beacon
+func (self *CassClient) PostDeployment(deployment *Deployment) {
+	// handle MessageId or Message fields appropriately
+	if deployment.MessageId {
+		// fetch message from id
+	} else {
+		// create message from msg struct
+	}
+	// take list of beacon names, write the new deployname to them all
+	for _, bName := range deployment.BeaconNames {
+
+	}
+
 }
 
 // FetchDeployment uses the deployments materialized view to gather a list of beacons associated with a deployment.
 // func FetchDeployment() {}
-
-// Crud ops for messages
-
-type User struct {
-	id    uuid
-	email string
-}
