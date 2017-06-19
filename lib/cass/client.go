6dd98001ebcd572f9ca309ce6cf55811f2d4cff5
owen <ow.diehl@gmail.com>
2017-06-19 12:14:34 -0400

    adds deployment_metadata cassandra table, writes out PostDeployment logic

--- lib/cass/client.go
+++ lib/cass/client.go
@@ -44,7 +44,16 @@ type Deployment struct {
 	BeaconNames []string
 }
 
-type CassClient struct{}
+// DeploymentMetadata mirrors the need for a 'dashboard' level overview of beacon deployments.
+type DeploymentMetadata struct {
+	UserId      gocql.UUID `cql:"user_id"`
+	DeployName  string     `cql:"deploy_name"`
+	MessageName string     `cql:"message_name"`
+}
+
+type CassClient struct {
+	Sess *gocql.Session
+}
 
 // UpsertResult is a wrapper type, including a possible batch & error. It can be used as the return value for batched or unbatched DML statements
 type UpsertResult struct {
@@ -60,6 +69,9 @@ func (self *CassClient) FetchUser(u *User, batch *gocql.Batch)               {}
 // Beacons ------------------------------------------------------------------------------
 
 func (self *CassClient) CreateBeacons(beacons []*Beacon, batch *gocql.Batch) UpsertResult {}
+
+// UpdateBeacons
+// Note: UpdateBeacons must use an if exists clause to prevent errors like inserting a beacon which a user does not own.
 func (self *CassClient) UpdateBeacons(beacons []*Beacon, batch *gocql.Batch) UpsertResult {}
 func (self *CassClient) FetchBeacons(beacons []*Beacon) (*Beacon, error)                  {}
 
@@ -70,26 +82,31 @@ func (self *CassClient) ChangeMessageDeployments(m *Message, additions *Message.
 }
 func (self *CassClient) FetchMessage(m *Message, batch *gocql.Batch) (*Message, error) {}
 
+// DeploymentMetadata
+func (self *CassClient) PostDeploymentMetadata(md *DeploymentMetadata, batch *gocql.Batch) UpsertResult {
+}
+
 // Deployments ------------------------------------------------------------------------------
 
 // PostDeployment writes the current deployname to every beacon in the list via an update clause, causing any now-invalid records in the deployments materialized view (on top of beacons) to be deleted via a partition drop (& subsequently recreated)
-// Must write Deployment to every beacon
+// Must write Deployment to every beacon, deploy_name to any message used, and bNames/messageName/deployName to deployments_metadata
 func (self *CassClient) PostDeployment(deployment *Deployment) UpsertResult {
 	batch := gocql.NewBatch(gocql.LoggedBatch)
-	// Note, should handle all these in batch.
 	// handle MessageName or Message fields appropriately
 	if deployment.MessageName {
 		// fetch message from id
-		msg, fetchErr := self.FetchMessage(&Message{Name: deployment.MessageName})
+		msg, fetchErr := self.FetchMessage(&Message{Name: deployment.MessageName, UserId: deployment.UserId})
 		if fetchErr != nil {
 			return nil, fetchErr
 		}
 
-		// assign message into deployment
-		deployment.Message = msg
+		// Need to UPDATE the message with the new deployment_name (append to set)
+		additions := Message.Deployments{deployment.DeployName}
+		self.ChangeMessageDeployments(&Message{UserId: deployment.UserId, Name: deployment.MessageName}, &additions, nil, batch)
 	} else {
 		// Otherwise, create a new message from the provided Message
 		deployment.Message.Deployments = []string{deployment.DeployName}
+		deployment.Message.UserId = deployment.UserId
 		self.CreateMessage(deployment.Message, batch)
 
 	}
@@ -106,7 +123,23 @@ func (self *CassClient) PostDeployment(deployment *Deployment) UpsertResult {
 
 	self.UpdateBeacons(bkns, batch)
 
+	// update metadata
+	deploymentMeta := DeploymentMetadata{
+		UserId:     deployment.UserId,
+		DeployName: deployment.DeployName,
+		// message could be provided as a reference (MessageName) or as a new Message object
+		MessageName: deployment.MessageName || deployment.Message.Name,
+	}
+
+	self.PostDeploymentMetadata(deploymentMeta, batch)
+
 	//Execute batch w/ context & return res
+	res := UpsertResult{
+		Err:   self.Sess.ExecuteBatch(batch),
+		Batch: batch,
+	}
+
+	return res
 }
 
 // FetchDeployment uses the deployments materialized view to gather a list of beacons associated with a deployment.
