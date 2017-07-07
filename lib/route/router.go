3ffdfce044326227460afa44d858e4c2f2f1ce9b
owen <ow.diehl@gmail.com>
2017-07-07 12:39:26 -0400

    router lib now returns http.Handler & adds some functionality for handler composition

--- lib/route/router.go
+++ lib/route/router.go
@@ -1,8 +1,15 @@
 package route
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"github.com/urfave/negroni"
+	"net/http"
+)
+
+var (
+	CorsHandler = cors.AllowAll()
 )
 
 type Endpoint struct {
@@ -18,11 +25,12 @@ type Router struct {
 	DefaultMiddleware []negroni.Handler
 	SubRoutes         []*Router
 	Endpoints         []*Endpoint
+	Name              string
 }
 
 func (r *Router) build(rootRouter *mux.Router, prependMiddleware []negroni.Handler) {
 	// build a new router from the path prefix, upon which all subsequent method routs will be mounted
-	r.Router = rootRouter.PathPrefix(r.Path).Subrouter().StrictSlash(true)
+	r.Router = rootRouter.PathPrefix(r.Path).Subrouter()
 
 	// inherit default middleware from parent
 	concatedDefaultMiddleware := make([]negroni.Handler, 0, len(prependMiddleware)+len(r.DefaultMiddleware))
@@ -35,12 +43,9 @@ func (r *Router) build(rootRouter *mux.Router, prependMiddleware []negroni.Handl
 		// allow a specified subpath to be handled on the same router for convenience, i.e. GET /item/:id can use a router on /item with a subpath /:id
 		sPath := endpoint.SubPath
 
-		if sPath == "" {
-			sPath = "/"
-		}
-
 		handler := negroni.New(r.DefaultMiddleware...).With(endpoint.Handlers...)
-		r.Router.Handle(sPath, handler).Methods(endpoint.Method)
+		fmtStr := fmt.Sprintf("\n\trPath: %+v\n\trName: %+v\n\tsPath: %+v\n\tmethod: %+v\n\n", r.Path, r.Name, sPath, endpoint.Method)
+		r.Router.Handle(sPath, handler).Methods(endpoint.Method).Name(fmtStr)
 	}
 
 	//recursively build subroutes
@@ -50,10 +55,24 @@ func (r *Router) build(rootRouter *mux.Router, prependMiddleware []negroni.Handl
 }
 
 // BuildRouter recursively builds all routes & related middleware via endpoints, returning the resulting mux router.
-func BuildRouter(root *Router) *Router {
+func BuildRouter(root *Router, initRouter *mux.Router) *Router {
 	// base case, must instantiate a new router
+	if initRouter == nil {
+		initRouter = mux.NewRouter()
+	}
+
 	// recursive call to build all deps
-	root.build(mux.NewRouter(), nil)
+	root.build(initRouter, nil)
 	return root
 
+}
+
+func Encase(initHandler http.Handler, prependMiddlewares ...http.Handler) http.Handler {
+	resHandler := negroni.New()
+	for _, h := range prependMiddlewares {
+		resHandler.UseHandler(h)
+	}
+
+	resHandler.UseHandler(initHandler)
+	return resHandler
 }
