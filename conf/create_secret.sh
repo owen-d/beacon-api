2d9905f12337d179959b17101c1fd617052e512b
owen-d <ow.diehl@gmail.com>
2017-08-14 13:33:37 -0400

    idempotent secret creation

--- conf/create_secret.sh
+++ conf/create_secret.sh
@@ -1,3 +1,22 @@
-kubectl -n api create secret generic v1api-configs \
+#!/bin/bash
+set -e
+
+if [[ -n $1 ]]
+then
+    SECRET_NAME=$1
+fi
+
+if [[ -n $2 ]]
+then
+    SECRET_NAMESPACE=$2
+fi
+
+
+
+kubectl create secret generic ${SECRET_NAME:=v1api-configs} \
         --from-file=./settings/config.json \
-        --from-file=./settings/gcp-credentials.json
+        --from-file=./settings/gcp-credentials.json \
+        --dry-run \
+        --save-config \
+        -o yaml \
+        | kubectl apply -n ${SECRET_NAMESPACE:=api} -f -
