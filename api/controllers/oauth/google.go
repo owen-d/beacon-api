194ff115b3368d446f0cc3a91371a1cbc82d216a
owen <ow.diehl@gmail.com>
2017-08-17 14:50:16 -0400

    functions for exchanging google oauth tokens & fetching users

--- api/controllers/oauth/google.go
+++ api/controllers/oauth/google.go
@@ -3,12 +3,20 @@ package oauth
 import (
 	"encoding/binary"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/owen-d/beacon-api/config"
 	"github.com/owen-d/beacon-api/lib/crypt"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"io/ioutil"
+	"net/http"
 	"time"
+)
+
+const (
+	userinfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo"
 )
 
 func NewOAuthConf(vars *config.OAuth, env string) *oauth2.Config {
@@ -27,10 +35,48 @@ func NewOAuthConf(vars *config.OAuth, env string) *oauth2.Config {
 	}
 }
 
-type GoogleAuth interface{}
+type GoogleAuth interface {
+	HandleAuth(http.ResponseWriter, *http.Request, http.HandlerFunc)
+}
 
 type GoogleAuthMethods struct {
 	OAuth *oauth2.Config
+	Coder *crypt.OmniCrypter
+}
+
+// Exchange validates a state string & exchanges the code for a token
+func (self *GoogleAuthMethods) exchange(state string, code string) (*oauth2.Token, error) {
+	if validationErr := ValidateState(self.Coder, state); validationErr != nil {
+		return nil, validationErr
+	}
+
+	return self.OAuth.Exchange(oauth2.NoContext, code)
+}
+
+// getUser will use a token to retrieve a corresponding user
+func (self *GoogleAuthMethods) getUser(tok *oauth2.Token) (*GoogleUser, error) {
+	client := self.OAuth.Client(oauth2.NoContext, tok)
+
+	resp, fetchErr := client.Get(userinfoEndpoint)
+	if fetchErr != nil {
+		return nil, fetchErr
+	}
+	defer resp.Body.Close()
+
+	data, readErr := ioutil.ReadAll(resp.Body)
+	fmt.Printf(string(data))
+
+	if readErr != nil {
+		return nil, readErr
+	}
+
+	user := &GoogleUser{}
+	return user, json.Unmarshal(data, user)
+
+}
+
+// func to handle redirect w/ state & code params. validate state & exchange code for user
+func (self *GoogleAuthMethods) HandleAuth(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 }
 
 func (self *GoogleAuthMethods) getLoginURL(state string) string {
@@ -79,4 +125,13 @@ func ValidateState(decoder *crypt.OmniCrypter, sig string) error {
 		return nil
 	}
 
+}
+
+type GoogleUser struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	Gender        string `json:"gender"`
 }
