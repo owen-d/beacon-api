30c185a1b14ee601a102585e8d8f60f324a78352
owen-d <ow.diehl@gmail.com>
2017-08-30 10:14:06 -0400

    adds update beacon deployments route, currently only updates data in cassandra (not google proximity api)

--- api/controllers/beacons/beacons.go
+++ api/controllers/beacons/beacons.go
@@ -8,11 +8,13 @@ import (
 	"github.com/owen-d/beacon-api/lib/route"
 	"github.com/owen-d/beacon-api/lib/validator"
 	"github.com/urfave/negroni"
+	"io/ioutil"
 	"net/http"
 )
 
 type BeaconRoutes interface {
 	GetBeacons(http.ResponseWriter, *http.Request, http.HandlerFunc)
+	ChangeDeployments(http.ResponseWriter, *http.Request, http.HandlerFunc)
 	// RegisterBeacons(http.ResponseWriter, *http.Request, http.HandlerFunc)
 	// DeregisterBeacons(http.ResponseWriter, *http.Request, http.HandlerFunc)
 	// UpdateBeacons(http.ResponseWriter, *http.Request, http.HandlerFunc)
@@ -26,6 +28,33 @@ type BeaconMethods struct {
 
 type BeaconResponse struct {
 	Beacons []*cass.Beacon `json:"beacons"`
+}
+
+type IncBeacons struct {
+	Beacons []*cass.Beacon `json:"beacons"`
+}
+
+func (self *IncBeacons) Validate(r *http.Request) ([]*cass.Beacon, *validator.RequestErr) {
+	jsonBody, readErr := ioutil.ReadAll(r.Body)
+
+	if readErr != nil {
+		return nil, &validator.RequestErr{400, "invalid json"}
+	}
+
+	unmarshalErr := json.Unmarshal(jsonBody, self)
+
+	if unmarshalErr != nil {
+		return nil, &validator.RequestErr{Status: 400, Message: "invalid json"}
+	}
+
+	bindings := r.Context().Value(jwt.JWTNamespace).(*jwt.Bindings)
+
+	// potentially overwrite malicious userId
+	for _, bkn := range self.Beacons {
+		bkn.UserId = bindings.UserId
+	}
+
+	return self.Beacons, nil
 }
 
 func (self *BeaconMethods) GetBeacons(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
@@ -46,11 +75,49 @@ func (self *BeaconMethods) GetBeacons(rw http.ResponseWriter, r *http.Request, n
 	rw.Write(data)
 }
 
+func (self *BeaconMethods) ChangeDeployments(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	bkns, validationErr := (&IncBeacons{}).Validate(r)
+
+	if validationErr != nil {
+		validationErr.Flush(rw)
+		return
+	}
+
+	additions := make([]*cass.Beacon, 0)
+	removals := make([]*cass.Beacon, 0)
+
+	for _, bkn := range bkns {
+		if bkn.DeployName == "" {
+			removals = append(removals, bkn)
+		} else {
+			additions = append(additions, bkn)
+		}
+	}
+
+	removalRes := self.CassClient.RemoveBeaconsDeployments(removals)
+	additionRes := self.CassClient.UpdateBeacons(additions)
+
+	if removalRes.Err != nil {
+		(&validator.RequestErr{Status: 500, Message: removalRes.Err.Error()}).Flush(rw)
+		return
+	}
+
+	if additionRes.Err != nil {
+		(&validator.RequestErr{Status: 500, Message: additionRes.Err.Error()}).Flush(rw)
+		return
+	}
+}
+
 func (self *BeaconMethods) Router() *route.Router {
 	endpoints := []*route.Endpoint{
 		&route.Endpoint{
 			Method:   "GET",
 			Handlers: []negroni.Handler{negroni.HandlerFunc(self.GetBeacons)},
+		},
+		&route.Endpoint{
+			Method:   http.MethodPut,
+			Handlers: []negroni.Handler{negroni.HandlerFunc(self.ChangeDeployments)},
+			SubPath:  "/deployments",
 		},
 	}
 
